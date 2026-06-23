4fbce5a36e7d0efa80ab99e8287d10d8537deec3
kristantapp <kristant.app@gmail.com>
2026-06-23 09:08:00 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'ylm-presensi-v1';
+const urlsToCache = [
+  '/',                // Membaca halaman utama otomatis di Vercel
+  '/index.html',
+  '/manifest.json',
+  '/Logo 192YLM.png',
+  '/Logo 512YLM.png',
+  '/Logo 180YLM.png'
+];
+
+// Install Service Worker dan simpan aset ke cache browser
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Membuka cache dan menyimpan aset...');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Ambil data dari cache jika sedang offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Gunakan aset dari cache jika ada
+        }
+        return fetch(event.request); // Jika tidak ada, ambil dari internet
+      })
+  );
+});
